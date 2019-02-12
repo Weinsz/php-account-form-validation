9dfb50265e404a0bba75cdd4d4dab60ba6460b7e
Vincenzo Siano <snipervince01@gmail.com>
2019-02-12 18:12:24 +0100

    Added Hashing methods using MySQLi and minor changes

--- database_mysqli.php
+++ database_mysqli.php
@@ -1,10 +1,15 @@
 <?php
 
 // Define the connection infos
-$servername = "your_host_ip";
-$username = "your_user";
-$password = "your_password";
-$database = "your_db_name";
+// $servername = "your_host_ip";
+// $username = "your_user";
+// $password = "your_password";
+// $database = "your_db_name";
+
+$servername = "localhost";
+$username = "root";
+$password = "L0g1c001";
+$database = "accounts";
 
 // Instantiate the connection to the database
 $connection = new mysqli($servername, $username, $password, $database);
@@ -12,7 +17,6 @@ $connection = new mysqli($servername, $username, $password, $database);
 // Check the connection
 if($connection->connect_error)
     die("Connection failed: " . $connection->connect_error . "with error: " . $connection->connect_errno);
-
 echo "Connected successfully";
 
 // Check if a row containing the 'email' or 'password' value sent through the form already exists
@@ -23,35 +27,50 @@ if(mysqli_num_rows($result) > 0)
 
 // Check if the submit button has been clicked
 if(isset($_POST['send'])) {
-    // Assign a default value if an input box hasn't been filled
-    if(empty($_POST['firstname'])) {
-        $_POST['firstname'] = 'NoName';
-    }
-    if(empty($_POST['lastname'])) {
-        $_POST['lastname'] = 'NoLastname';
-    }
-    if(empty($_POST['gender'])) {
-        $_POST['gender'] = 'M';
-    }
-    if(empty($_POST['country'])) {
-        $_POST['country'] = 'NoCountry';
-    }
-    if(empty($_POST['city'])) {
-        $_POST['city'] = 'NoCity';
-    }
-    if(empty($_POST['birth_date'])) {
-        $_POST['birthdate'] = NULL;
-    }
 
-    // Send to the database the values submitted via the form inserting them into a new row
-    $sql_query = "INSERT INTO account_info ". 
-                 "(first_name, last_name, email, username, password, ".
-                 "signup_date, gender, country, city, birth_date) ".
-                 "VALUES ('{$_POST["firstname"]}', '{$_POST["lastname"]}', '{$_POST["email"]}', '{$_POST["username"]}', '{$_POST["pwd"]}', ".
-                 "NOW(), '{$_POST['gender']}', '{$_POST["country"]}', '{$_POST["city"]}', '{$_POST["birthdate"]}' )";
-    
-    $result = mysqli_query($connection, $sql_query);
-    echo "<br>" . $result;
+    // Assign a default value if an input box hasn't been filled
+    if(empty($_POST['firstname']))
+        $_POST['firstname'] = 'NoName';
+
+    if(empty($_POST['lastname']))
+        $_POST['lastname'] = 'NoLastname';
+
+    if(empty($_POST['gender']))
+        $_POST['gender'] = 'M';
+
+    if(empty($_POST['country']))
+        $_POST['country'] = 'NoCountry';
+
+    if(empty($_POST['city']))
+        $_POST['city'] = 'NoCity';
+
+    if(empty($_POST['birth_date']))
+        $_POST['birthdate'] = NULL;
+
+
+    // Hash the password using the CRYPT_BLOWFISH algorithm whose result will always be a 60 character string
+    $hashed_password = password_hash($_POST["pwd"], PASSWORD_BCRYPT);
+
+    // Check if the password inserted in the input box is the same of the hashed one
+    if(password_verify($_POST['pwd'], $hashed_password)) {
+        echo "<br>Valid password inserted<br>";
+
+        // Send to the database the values submitted via the form inserting them into a new row
+        $sql_query = "INSERT INTO account_info ". 
+                    "(first_name, last_name, email, username, password, ".
+                    "signup_date, gender, country, city, birth_date) ".
+                    "VALUES ('{$_POST["firstname"]}', '{$_POST["lastname"]}', '{$_POST["email"]}', '{$_POST["username"]}', '{$hashed_password}', ".
+                    "NOW(), '{$_POST['gender']}', '{$_POST["country"]}', '{$_POST["city"]}', '{$_POST["birthdate"]}' )";
+        
+        if($result = mysqli_query($connection, $sql_query))
+            echo "Query sent successfully to the Database";
+        else 
+            echo "Something went wrong during the query send to the Database";
+
+    }
+    else
+        die("Invalid password inserted. Something went wrong");
+
 }
 
 // Close the connection (not necessary because it is always closed automatically)
